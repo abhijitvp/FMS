11d586c2f01b9afeb89dcaf037a18902dd77fd1d
Abhijit Patil <Abhijit.Patil1@wolterskluwer.com>
2023-03-21 16:45:12 +0530

    .

--- RemoveFiles/RemoveFiles/Program.cs
+++ RemoveFiles/RemoveFiles/Program.cs
@@ -17,6 +17,7 @@ namespace RemoveFiles
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SearchText());
+
         }
     }
 }
